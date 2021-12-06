c9f36371fa30e74a37de2ad56339621189bfc6bf
juandavidHM <91087000+juandavidHM@users.noreply.github.com>
2021-12-06 09:13:34 -0500

    Update App.js

--- App.js
+++ App.js
@@ -11,22 +11,24 @@ function App() {
           <div className='contenedor2'>
             <img className='imagen' src={imagen} alt='banner'>
             </img>
-            <h1>INICIA SESION</h1>
-            <div className='inicio'>
-              <div className='correo'>
-                <h5 >Correo</h5>
-                <input type="email" className="email" id="email" class="input" ></input>
+            <from classname='login_from'>
+              <h1>INICIA SESION</h1>
+              <div className='inicio'>
+                <div className='correo'>
+                  <h5 >Correo</h5>
+                  <input type="email" className="email" id="email" class="input" ></input>
+                </div>
+               </div>
+               <div className='inicio'>
+                <div className='contraseña'>
+                  <h5 >Contraseña</h5>
+                  <input type="password" name="password" id="password" class="input"></input>
+                </div>
               </div>
-             </div>
-             <div className='inicio'>
-              <div className='contraseña'>
-                <h5 >Contraseña</h5>
-           	    <input type="password" name="password" id="password" class="input"></input>
-              </div>
-            </div>
-            <a href='#' className='uno'>¿Has olvidado tu contraseña?.</a>
-            <a href='#' className='dos'>¿Ya te has registrado?.</a>
-            <button className='boton' type="submit">Iniciar Sesión</button>
+              <a href='#' className='uno'>¿Has olvidado tu contraseña?.</a>
+              <a href='#' className='dos'>¿Ya te has registrado?.</a>
+              <button className='boton' type="submit">Iniciar Sesión</button>
+            </from>
           </div>
     </div>
   );
